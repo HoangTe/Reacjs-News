9dff714b921f01aeeb9ffd5fc1d5dcce825e254e
HoangTe <doanhuyhoang11a2ntt2020@gmail.com>
2024-08-22 21:44:42 +0700

    first

---
+++ src/components/Copyright.js
@@ -0,0 +1,13 @@
+import './../../src/assets/css/Copyright.css'
+
+const Copyright = () => {
+    return (
+        <div className="copy bg-dark text-white text-center py-3">
+            <div className="container">
+                <p className="mb-0">Copyright © 2024; Designed by <span>Doan Huy Hoang</span> </p>
+            </div>
+        </div>
+    );
+}
+
+export default Copyright;
---
+++ src/components/Footer.js
@@ -0,0 +1,26 @@
+import './../../src/assets/css/Footer.css';
+
+const Footer = () => {
+    return(
+        <footer className="footer bg-dark text-white text-center py-4">
+            <div className="container">
+                <div className="social-icons mb-3">
+                    <a href="#" className="icon"><i class="bi bi-facebook"></i></a>
+                    <a href="#" className="icon"><i class="bi bi-instagram"></i></a>
+                    <a href="#" className="icon"><i class="bi bi-twitter"></i></a>
+                    <a href="#" className="icon"><i class="bi bi-google"></i></a>
+                    <a href="#" className="icon"><i class="bi bi-youtube"></i></a>
+                </div>
+                <div className="footer-links">
+                    <a href="#" className="text-white mx-3">Home</a>
+                    <a href="#" className="text-white mx-3">News</a>
+                    <a href="#" className="text-white mx-3">About</a>
+                    <a href="#" className="text-white mx-3">Contact Us</a>
+                    <a href="#" className="text-white mx-3">Our Team</a>
+                </div>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
